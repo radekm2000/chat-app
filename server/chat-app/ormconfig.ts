5453946cdfd6cff66f88bbd753c79fa6fa82c02f
Radek <104318242+radekm2000@users.noreply.github.com>
2023-12-04 17:51:41 +0100

    add: production database connection options and scripts to migrations

--- server/chat-app/ormconfig.ts
+++ server/chat-app/ormconfig.ts
@@ -7,22 +7,39 @@ import { Avatar } from 'src/utils/entities/avatar.entity';
 import { ResetPasswordToken } from 'src/utils/entities/resetPasswordToken.entity';
 import { FriendRequest } from 'src/utils/entities/friendRequest.entity';
 import { Friend } from 'src/utils/entities/friend.entity';
+import { DataSource } from 'typeorm';
 
-export const config: PostgresConnectionOptions = {
-  type: 'postgres',
-  database: process.env.POSTGRES_DB,
-  host: 'localhost',
-  port: 5432,
-  username: process.env.POSTGRES_USER,
-  password: process.env.POSTGRES_PASSWORD,
-  entities: [
-    User,
-    Conversation,
-    Message,
-    Avatar,
-    ResetPasswordToken,
-    FriendRequest,
-    Friend,
-  ],
-  synchronize: true,
-};
+export let config: PostgresConnectionOptions;
+if (process.env.IS_PRODUCTION === 'true') {
+  config = {
+    type: 'postgres',
+    database: 'railway',
+    host: 'monorail.proxy.rlwy.net',
+    port: 51208,
+    username: 'postgres',
+    password: 'daE36a*BgA36G4CEe2BaDCBFFCdDaB*B',
+    entities: ['dist/**/*.entity.js'],
+    migrations: ['dist/db/migrations/*.js'],
+  };
+} else {
+  config = {
+    type: 'postgres',
+    database: process.env.POSTGRES_DB,
+    host: 'localhost',
+    port: 5432,
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    entities: [
+      User,
+      Conversation,
+      Message,
+      Avatar,
+      ResetPasswordToken,
+      FriendRequest,
+      Friend,
+    ],
+    synchronize: true,
+  };
+}
+
+export const dataSource = new DataSource(config);
